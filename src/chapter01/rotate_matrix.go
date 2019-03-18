895004e5d50d86b89931ce7528bac6bc1ff56ba0
yomoda07 <kanji.yy@gmail.com>
2019-03-19 01:57:03 +0900

    Solve 1.7 Rotate Matrix

---
+++ src/chapter01/rotate_matrix.go
@@ -0,0 +1,35 @@
+package main
+
+// 1.7 Rotate Matrix
+// Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+// write a function to rotate the image by 90 degrees. Can you do this in place?
+func rotateMatrix(matrix [][]int) [][]int {
+	matrixLength := len(matrix)
+	rotatedMatrix := make([][]int, matrixLength)
+	for layer := 0; row < matrixLength; row++ {
+		rotatedMatrix[layer] = make([]int, matrixLength)
+		for i := 0; i < matrixLength; i++ {
+			rotatedMatrix[layer][i] = matrix[matrixLength-i-1][row]
+		}
+	}
+	return rotatedMatrix
+}
+
+// this is more efficient than above by not allocating memory for copied array
+func betterRotateMatrix(matrix [][]int) [][]int {
+	matrixLength := len(matrix)
+	// start from outermost layer and goes to inside
+	for layer := 0; layer < matrixLength/2; layer++ {
+		first := layer
+		last := matrixLength - 1 - first
+		for i := first; i < last; i++ {
+			offset := i - first
+			top := matrix[first][i]
+			matrix[first][i] = matrix[last-offset][first]     // left -> top
+			matrix[last-offset][first] = matrix[last][last-offset] // bottom -> left
+			matrix[last][last-offset] = matrix[i][last]       // right -> bottom
+			matrix[i][last] = top                             // top -> right
+		}
+	}
+	return matrix
+}
---
+++ src/chapter01/rotate_matrix_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRotateMatrix(t *testing.T) {
+	testCases := []struct {
+		matrix   [][]int
+		expected [][]int
+	}{
+		{
+			[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			[][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}},
+		},
+		{
+			[][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}},
+			[][]int{{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}},
+		},
+	}
+	for _, tc := range testCases {
+		result := rotateMatrix(tc.matrix)
+		if !reflect.DeepEqual(tc.expected, result) {
+			t.Fatalf("failed test expected: %v, got: %v, params: %v", tc.expected, result, tc.matrix)
+		}
+	}
+}
+
+func TestBetterRotateMatrix(t *testing.T) {
+	testCases := []struct {
+		matrix   [][]int
+		expected [][]int
+	}{
+		{
+			[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			[][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}},
+		},
+		{
+			[][]int{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}},
+			[][]int{{13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}, {16, 12, 8, 4}},
+		},
+	}
+	for _, tc := range testCases {
+		result := betterRotateMatrix(tc.matrix)
+		if !reflect.DeepEqual(tc.expected, result) {
+			t.Fatalf("failed test expected: %v, got: %v, params: %v", tc.expected, result, tc.matrix)
+		}
+	}
+}
